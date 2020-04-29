9c3b671b3412b22dc445926618f2c663ff4dd97c
Lisa Ostman <peolina@live.com.au>
2020-04-29 20:52:27 +1000

    herku

---
+++ client/build/precache-manifest.79e557e667edfb5b0507334a37ccf115.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7558b710fdb161921ad693c436a71c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ecc00eee944e802625ba",
+    "url": "/static/css/main.e304e6ea.chunk.css"
+  },
+  {
+    "revision": "ccf72ee4d76760833fb1",
+    "url": "/static/js/2.77cb4d22.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/static/js/2.77cb4d22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecc00eee944e802625ba",
+    "url": "/static/js/main.6d90ac11.chunk.js"
+  },
+  {
+    "revision": "81df08110b691a8a92f6",
+    "url": "/static/js/runtime-main.bbaf3f1a.js"
+  },
+  {
+    "revision": "8159ff2c4564a4003d67c96ac575aa7e",
+    "url": "/static/media/Adventure.8159ff2c.png"
+  },
+  {
+    "revision": "eebbd18374fdbb854dfafea86db7adf4",
+    "url": "/static/media/BalooTamma2-Bold.eebbd183.ttf"
+  },
+  {
+    "revision": "891276b036b0f4ea426ecfc0cee26b7b",
+    "url": "/static/media/BalooTamma2-SemiBold.891276b0.ttf"
+  },
+  {
+    "revision": "50e8495884e2b496c7e6e52852a7052d",
+    "url": "/static/media/Cafe.50e84958.png"
+  },
+  {
+    "revision": "99a9c14a336941ef5b6cf932d1df2263",
+    "url": "/static/media/Market.99a9c14a.png"
+  },
+  {
+    "revision": "6b93c3974606cf403d69a721707699b9",
+    "url": "/static/media/Retail.6b93c397.png"
+  },
+  {
+    "revision": "4cab3c45f43e7649a6336552770c1989",
+    "url": "/static/media/background.4cab3c45.png"
+  },
+  {
+    "revision": "c9d5aec8a91c37b640abba88a5af508d",
+    "url": "/static/media/logo.c9d5aec8.png"
+  }
+]);
