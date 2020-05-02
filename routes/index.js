33a11a629d8eb6e890fdc5a1aacf0059ea72bfba
Lisa Ostman <peolina@live.com.au>
2020-05-02 11:44:13 +1000

    m

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,4 @@ const apiRoutes = require("../controllers/databaseController.js");
 // // API Routes
 router.use(apiRoutes);
 
-// // If no API routes are hit, send the React app
-// router.use(function(req, res) {
-//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// });
-
 module.exports = router;
