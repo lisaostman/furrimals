776af34a46f466380e0ade66804982b2933d529f
Lisa Ostman <peolina@live.com.au>
2020-05-02 11:32:24 +1000

    heroku please work

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom' //don't need to specify localhost url in axios http address
+import { BrowserRouter } from 'react-router-dom' 
 import './fonts/BalooTamma2-SemiBold.ttf'
 
 //style
--- controllers/databaseController.js
+++ controllers/databaseController.js
@@ -4,19 +4,18 @@ var router = express.Router();
 
 var database = require("../models/database.js");
 
-router.post("https://furrimals.herokuapp.com/api/create", function(req, res) {
+router.post("/api/create", function(req, res) {
   database.create("furrimal_user",
     [
     "email"
   ], [
     req.body.email
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.post("https://furrimals.herokuapp.com/api/caught", function(req, res) {
+router.post("/api/caught", function(req, res) {
   database.create("furrimal_caught",
     [
     "userId",
@@ -25,12 +24,11 @@ router.post("https://furrimals.herokuapp.com/api/caught", function(req, res) {
     req.body.userId,
     req.body.animalId
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-router.post("https://furrimals.herokuapp.com/api/addfriend", function(req, res) {
+router.post("/api/addfriend", function(req, res) {
   database.create("furrimal_friends",
     [
     "firstFriend",
@@ -39,29 +37,11 @@ router.post("https://furrimals.herokuapp.com/api/addfriend", function(req, res) 
     req.body.firstFriend,
     req.body.secondFriend
   ], function(result) {
-    // Send back the ID of the new quote
     res.json({ id: result.insertId });
   });
 });
 
-// router.put("/api/cats/:id", function(req, res) {
-//   var condition = "id = " + req.params.id;
-
-//   console.log("condition", condition);
-
-//   cat.update({
-//     sleepy: req.body.sleepy
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
-
-router.get("https://furrimals.herokuapp.com/collection/api", function(req, res) {
+router.get("/collection/api", function(req, res) {
   let condition = req.query.email;
   console.log("reached router back!")
   database.all(condition, function(result) {
@@ -69,7 +49,7 @@ router.get("https://furrimals.herokuapp.com/collection/api", function(req, res) 
   });
 });
 
-router.get("https://furrimals.herokuapp.com/api/code", function(req, res) {
+router.get("/api/code", function(req, res) {
   let condition = req.query.code;
   console.log("reached router back!")
   database.shopCode(condition, function(result) {
@@ -77,7 +57,7 @@ router.get("https://furrimals.herokuapp.com/api/code", function(req, res) {
   });
 });
 
- router.get("https://furrimals.herokuapp.com/api/seconduser", function(req, res) {
+ router.get("/api/seconduser", function(req, res) {
    let condition = req.query.seconduser;
    console.log("reached router back!")
    database.findFriend(condition, function(result) {
@@ -85,7 +65,7 @@ router.get("https://furrimals.herokuapp.com/api/code", function(req, res) {
    });
  });
 
-router.get("https://furrimals.herokuapp.com/api/email", function(req, res) {
+router.get("/api/email", function(req, res) {
   let condition = req.query.email;
   console.log("reached router back!")
   database.userFinder(condition, function(result) {
@@ -93,7 +73,4 @@ router.get("https://furrimals.herokuapp.com/api/email", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
 module.exports = router;
-
-//http://localhost:3001/collection/api?email=example@live.com
