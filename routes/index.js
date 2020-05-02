b9d7f074b162050b2a399f2d011381237716cdcb
Lisa Ostman <peolina@live.com.au>
2020-05-02 11:45:32 +1000

    m

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("../controllers/databaseController.js");
 
-// // API Routes
 router.use(apiRoutes);
 
 module.exports = router;
